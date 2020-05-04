3794894711af00367e129d2cf1fd6012883bcce8
Arun Kumar Anaparthi <v-aruana@microsoft.com>
2020-05-03 23:51:06 -0700

    Updated adaptive card and message extension methods

--- Reflection/Bots/MessageExtension.cs
+++ Reflection/Bots/MessageExtension.cs
@@ -19,37 +19,77 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web
 {
     public class MessageExtension : TeamsActivityHandler
     {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            if (turnContext.Activity.Value != null)
+            {
+                // This was a message from the card.
+                //var obj = (JObject)turnContext.Activity.Value;
+                //var answer = obj["Answer"]?.ToString();
+                //var choices = obj["Choices"]?.ToString();
+                //await turnContext.SendActivityAsync(MessageFactory.Text($"{turnContext.Activity.From.Name} answered '{answer}' and chose '{choices}'."), cancellationToken);
+                var reply = Activity.CreateMessageActivity();
+                string card = "{\r\n    \"type\": \"AdaptiveCard\",\r\n    \"version\": \"1.0\",\r\n    \"body\": [\r\n        {\r\n            \"type\": \"Image\",\r\n            \"altText\": \"\",\r\n            \"url\": \"https://1f0bd229.ngrok.io/images/Firstresponsecolor.png\",\r\n            \"width\": \"\"\r\n        },\r\n        {\r\n            \"type\": \"ColumnSet\",\r\n            \"columns\": [\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"items\": [\r\n                        {\r\n                            \"type\": \"Image\",\r\n                            \"altText\": \"\",\r\n                            \"url\": \"https://1f0bd229.ngrok.io/images/ref1.png\"\r\n                        },\r\n                        {\r\n                            \"type\": \"TextBlock\",\r\n                            \"text\": \"1\"\r\n                        }\r\n                    ],\r\n                    \"width\": \"stretch\"\r\n                },\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"items\": [\r\n                        {\r\n                            \"type\": \"Image\",\r\n                            \"altText\": \"\",\r\n                            \"url\": \"https://1f0bd229.ngrok.io/images/ref2.png\"\r\n                        },\r\n                        {\r\n                            \"type\": \"TextBlock\",\r\n                            \"text\": \"1\"\r\n                        }\r\n                    ],\r\n                    \"width\": \"stretch\"\r\n                },\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"items\": [\r\n                        {\r\n                            \"type\": \"Image\",\r\n                            \"altText\": \"1\",\r\n                            \"url\": \"https://1f0bd229.ngrok.io/images/ref3.png\"\r\n                        },\r\n                        {\r\n                            \"type\": \"TextBlock\",\r\n                            \"text\": \"1\"\r\n                        }\r\n                    ],\r\n                    \"width\": \"stretch\"\r\n                },\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"items\": [\r\n                        {\r\n                            \"type\": \"Image\",\r\n                            \"altText\": \"\",\r\n                            \"url\": \"https://1f0bd229.ngrok.io/images/ref4.png\"\r\n                        },\r\n                        {\r\n                            \"type\": \"TextBlock\",\r\n                            \"text\": \"1\"\r\n                        }\r\n                    ],\r\n                    \"width\": \"stretch\"\r\n                },\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"items\": [\r\n                        {\r\n                            \"type\": \"Image\",\r\n                            \"altText\": \"\",\r\n                            \"url\": \"https://1f0bd229.ngrok.io/images/ref5.png\"\r\n                        },\r\n                        {\r\n                            \"type\": \"TextBlock\",\r\n                            \"text\": \"1\"\r\n                        }\r\n                    ],\r\n                    \"width\": \"stretch\"\r\n                }\r\n            ]\r\n        }\r\n    ],\r\n    \"$schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\"\r\n}";
+
+                AdaptiveCardParseResult result = AdaptiveCard.FromJson(card);
+                Attachment attachment = new Attachment()
+                {
+                    ContentType = AdaptiveCard.ContentType,
+                    Content = result.Card
+                };
+                reply.Attachments.Add(attachment);
+                await turnContext.SendActivityAsync(reply, cancellationToken);
+            }
+            else
+            {
+                // This is a regular text message.
+                await turnContext.SendActivityAsync(MessageFactory.Text($"Hello from the TeamsMessagingExtensionsActionPreviewBot."), cancellationToken);
+            }
+        }
         protected override async Task<MessagingExtensionActionResponse> OnTeamsMessagingExtensionSubmitActionAsync(ITurnContext<IInvokeActivity> turnContext, MessagingExtensionAction action, CancellationToken cancellationToken)
         {
             var val = JsonConvert.DeserializeObject<TaskInfo>(action.Data.ToString()).action;
             //var cardData = JsonConvert.DeserializeObject
+            string card = "{\r\n    \"type\": \"AdaptiveCard\",\r\n    \"version\": \"1.0\",\r\n    \"body\": [\r\n        {\r\n            \"type\": \"ColumnSet\",\r\n            \"columns\": [\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"width\": \"stretch\",\r\n                    \"items\": [\r\n                        {\r\n                            \"type\": \"TextBlock\",\r\n                            \"text\": \"Created by: Arun Kumar\"\r\n                        }\r\n                    ]\r\n                },\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"width\": \"stretch\",\r\n                    \"items\": [\r\n                        {\r\n                            \"type\": \"TextBlock\",\r\n                            \"text\": \"| Responses are public\"\r\n                        }\r\n                    ]\r\n                }\r\n            ]\r\n        },\r\n        {\r\n            \"type\": \"Container\",\r\n            \"items\": [\r\n                {\r\n                    \"type\": \"TextBlock\",\r\n                    \"text\": \"How are you felling about the test today?\"\r\n                }\r\n            ]\r\n        },\r\n        {\r\n            \"type\": \"ColumnSet\",\r\n            \"columns\": [\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"width\": \"stretch\",\r\n                    \"items\": [\r\n                        {\r\n                                \"type\": \"Image\",\r\n                                \"url\": \"https://1f0bd229.ngrok.io/images/1.png\",\r\n                                \"altText\":\"1\",\r\n                                \"selectAction\": {\r\n                                                    \"type\": \"Action.Submit\"\r\n                                                }\r\n                        }\r\n                    ]\r\n                },\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"width\": \"stretch\",\r\n                    \"items\": [\r\n                        {\r\n                                \"type\": \"Image\",\r\n                                \"url\": \"https://1f0bd229.ngrok.io/images/2.png\",\r\n                                \"altText\":\"2\",\r\n                                \"selectAction\": {\r\n                                                    \"type\": \"Action.Submit\"\r\n                                                }\r\n                        }\r\n                        \r\n                    ]\r\n                },\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"width\": \"stretch\",\r\n                    \"items\": [\r\n                        {\r\n                                \"type\": \"Image\",\r\n                                \"url\": \"https://1f0bd229.ngrok.io/images/3.png\",\r\n                                \"altText\":\"3\",\r\n                                \"selectAction\": {\r\n                                                    \"type\": \"Action.Submit\"\r\n                                                }\r\n                        }\r\n                    ]\r\n                },\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"width\": \"stretch\",\r\n                    \"items\": [\r\n                        {\r\n                                \"type\": \"Image\",\r\n                                \"url\": \"https://1f0bd229.ngrok.io/images/4.png\",\r\n                                \"altText\":\"4\",\r\n                                \"selectAction\": {\r\n                                                    \"type\": \"Action.Submit\"\r\n                                                }\r\n                        }\r\n                    ]\r\n                },\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"width\": \"stretch\",\r\n                    \"items\": [\r\n                        {\r\n                                \"type\": \"Image\",\r\n                                \"url\": \"https://1f0bd229.ngrok.io/images/5.png\",\r\n                                \"altText\":\"5\",\r\n                                \"selectAction\": {\r\n                                                    \"type\": \"Action.Submit\"\r\n                                                }\r\n                        }\r\n                    ]\r\n                }\r\n            ]\r\n        }\r\n    ],\r\n    \"actions\": [\r\n          {\r\n            \"type\": \"Action.Submit\",\r\n            \"title\": \"Remove Post\"\r\n          },\r\n          {\r\n            \"type\": \"Action.Submit\",\r\n            \"title\": \"Manage recurring posts\"\r\n          }\r\n        ],\r\n    \"$schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\"\r\n}";
 
-            switch (val)
-            {
-                case "reflection":
-                    return await OnTeamsMessagingExtensionFetchTaskAsync(turnContext, action, cancellationToken);
-                case "sendAdaptiveCard":
-                    return await CardHelper.SendNewPost(turnContext, action, cancellationToken, action.Data.ToString()); //create model for action data
-                case "Chaining":
-                    var response = new MessagingExtensionActionResponse()
-                    {
-                        Task = new TaskModuleContinueResponse()
-                        {
-                            Value = new TaskModuleTaskInfo()
-                            {
-                                Height = 550,
-                                Width = 780,
-                                Title = "Check the pulse on emotinal well-being",
-                                Url = "https://cd013f78.ngrok.io/ManageRecurringPosts"
-                            },
-                        },
-                    };
+            AdaptiveCardParseResult result = AdaptiveCard.FromJson(card);
 
-                    return response;
-                default:
-                    return null;
-            };
-            
+            var message = MessageFactory.Attachment(new Attachment { ContentType = AdaptiveCard.ContentType, Content = result.Card });
+            var channelId = turnContext.Activity.TeamsGetChannelId();
+
+
+            // THIS WILL WORK IF THE BOT IS INSTALLED. (SendActivityAsync will throw if the bot is not installed.)
+            await turnContext.SendActivityAsync(message, cancellationToken);
+
+            // await turnContext. TeamsCreateConversationAsync(channelId, message, cancellationToken);
+            return null;
+
+            //switch (val)
+            //{
+            //    case "reflection":
+            //        return await OnTeamsMessagingExtensionFetchTaskAsync(turnContext, action, cancellationToken);
+            //    case "sendAdaptiveCard":
+            //        return await CardHelper.SendNewPost(turnContext, action, cancellationToken, action.Data.ToString()); //create model for action data
+            //    case "Chaining":
+            //        var response = new MessagingExtensionActionResponse()
+            //        {
+            //            Task = new TaskModuleContinueResponse()
+            //            {
+            //                Value = new TaskModuleTaskInfo()
+            //                {
+            //                    Height = 550,
+            //                    Width = 780,
+            //                    Title = "Check the pulse on emotinal well-being",
+            //                    Url = "https://1f0bd229.ngrok.io/ManageRecurringPosts"
+            //                },
+            //            },
+            //        };
+
+            //        return response;
+            //    default:
+            //        return null;
+            //};
+
         }
 
         protected override async Task<MessagingExtensionActionResponse> OnTeamsMessagingExtensionFetchTaskAsync(ITurnContext<IInvokeActivity> turnContext, MessagingExtensionAction action, CancellationToken cancellationToken)
@@ -69,7 +109,7 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web
                         Height = 720,
                         Width = 900,
                         Title = "Invite people to share how they feel",
-                        Url = "https://cd013f78.ngrok.io/"
+                        Url = "https://1f0bd229.ngrok.io/"
                     },
                 },
             };
@@ -146,8 +186,19 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web
 
         protected async override Task OnTeamsMessagingExtensionCardButtonClickedAsync(ITurnContext<IInvokeActivity> turnContext, JObject cardData, CancellationToken cancellationToken)
         {
-        var reply = MessageFactory.Text("OnTeamsMessagingExtensionCardButtonClickedAsync Value: " + JsonConvert.SerializeObject(turnContext.Activity.Value));
-        await turnContext.SendActivityAsync(reply, cancellationToken);
+        //var reply = MessageFactory.Text("OnTeamsMessagingExtensionCardButtonClickedAsync Value: " + JsonConvert.SerializeObject(turnContext.Activity.Value));
+            var reply = Activity.CreateMessageActivity();
+            string card = "{\r\n    \"type\": \"AdaptiveCard\",\r\n    \"version\": \"1.0\",\r\n    \"body\": [\r\n        {\r\n            \"type\": \"Image\",\r\n            \"altText\": \"\",\r\n            \"url\": \"https://1f0bd229.ngrok.io/images/Firstresponsecolor.png\",\r\n            \"width\": \"\"\r\n        },\r\n        {\r\n            \"type\": \"ColumnSet\",\r\n            \"columns\": [\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"items\": [\r\n                        {\r\n                            \"type\": \"Image\",\r\n                            \"altText\": \"\",\r\n                            \"url\": \"https://1f0bd229.ngrok.io/images/ref1.png\"\r\n                        },\r\n                        {\r\n                            \"type\": \"TextBlock\",\r\n                            \"text\": \"1\"\r\n                        }\r\n                    ],\r\n                    \"width\": \"stretch\"\r\n                },\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"items\": [\r\n                        {\r\n                            \"type\": \"Image\",\r\n                            \"altText\": \"\",\r\n                            \"url\": \"https://1f0bd229.ngrok.io/images/ref2.png\"\r\n                        },\r\n                        {\r\n                            \"type\": \"TextBlock\",\r\n                            \"text\": \"1\"\r\n                        }\r\n                    ],\r\n                    \"width\": \"stretch\"\r\n                },\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"items\": [\r\n                        {\r\n                            \"type\": \"Image\",\r\n                            \"altText\": \"1\",\r\n                            \"url\": \"https://1f0bd229.ngrok.io/images/ref3.png\"\r\n                        },\r\n                        {\r\n                            \"type\": \"TextBlock\",\r\n                            \"text\": \"1\"\r\n                        }\r\n                    ],\r\n                    \"width\": \"stretch\"\r\n                },\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"items\": [\r\n                        {\r\n                            \"type\": \"Image\",\r\n                            \"altText\": \"\",\r\n                            \"url\": \"https://1f0bd229.ngrok.io/images/ref4.png\"\r\n                        },\r\n                        {\r\n                            \"type\": \"TextBlock\",\r\n                            \"text\": \"1\"\r\n                        }\r\n                    ],\r\n                    \"width\": \"stretch\"\r\n                },\r\n                {\r\n                    \"type\": \"Column\",\r\n                    \"items\": [\r\n                        {\r\n                            \"type\": \"Image\",\r\n                            \"altText\": \"\",\r\n                            \"url\": \"https://1f0bd229.ngrok.io/images/ref5.png\"\r\n                        },\r\n                        {\r\n                            \"type\": \"TextBlock\",\r\n                            \"text\": \"1\"\r\n                        }\r\n                    ],\r\n                    \"width\": \"stretch\"\r\n                }\r\n            ]\r\n        }\r\n    ],\r\n    \"$schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\"\r\n}";
+
+            AdaptiveCardParseResult result = AdaptiveCard.FromJson(card);
+            Attachment attachment = new Attachment()
+            {
+                ContentType = AdaptiveCard.ContentType,
+                Content = result.Card
+            };
+            reply.Attachments.Add(attachment);
+            await turnContext.SendActivityAsync(reply, cancellationToken);
+
 
             //return base.OnTeamsMessagingExtensionCardButtonClickedAsync(turnContext, cardData, cancellationToken);
         }
