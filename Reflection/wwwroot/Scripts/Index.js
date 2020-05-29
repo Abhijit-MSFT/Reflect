96a0e168f7b3709eb264297175c88c673af69a24
TarakaSivakoti <tarak.sivakoti@gmail.com>
2020-05-29 18:49:33 +0530

    Theme based remaining modal css are done
    Theme based remaining modal css are done

--- Reflection/wwwroot/Scripts/Index.js
+++ Reflection/wwwroot/Scripts/Index.js
@@ -3,44 +3,70 @@ var userobject = {};
 var accesstoken = "";
 $(document).ready(function () {
     microsoftTeams.initialize();
+    microsoftTeams.getContext(function (context) {
+        if (context.theme == "default") {
+            var head = document.getElementsByTagName("head")[0], // reference to document.head for appending/ removing link nodes
+                link = document.createElement("link"); // create the link node
+            link.setAttribute("href", "../CSS/Index.css");
+            link.setAttribute("rel", "stylesheet");
+            link.setAttribute("type", "text/css");
+            head.appendChild(link);
+        } else if (context.theme == "dark") {
+            var head = document.getElementsByTagName("head")[0],
+                link = document.createElement("link");
+            link.setAttribute("href", "../CSS/Index-dark.css");
+            link.setAttribute("rel", "stylesheet");
+            link.setAttribute("type", "text/css");
+            head.appendChild(link);
+        } else {
+            var head = document.getElementsByTagName("head")[0],
+                link = document.createElement("link");
+            link.setAttribute("href", "../CSS/Index-dark.css");
+            link.setAttribute("rel", "stylesheet");
+            link.setAttribute("type", "text/css");
+            head.appendChild(link);
+        }
+    });
     $.ajax({
         url: "/api/GetAccessTokenAsync",
         type: "Get",
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Accept': 'application/json'
+            "Content-Type": "application/x-www-form-urlencoded",
+            Accept: "application/json",
         },
         success: function (token) {
             accesstoken = token;
             microsoftTeams.getContext(function (context) {
-                if (token !== undefined && token !== null && token !== '') {
+                if (token !== undefined && token !== null && token !== "") {
                     GetUserDetails(context.userPrincipalName, token);
                     GetDefaultQuestions(context.userPrincipalName);
                 }
             });
-
+        },
+    });
+    $("body").click(function (e) {
+        if (e.target.className !== "form-control questioninput") {
+            $("#questionsblock").addClass("hidequestions");
+            $("#questionsblock").removeClass("showquestions");
         }
     });
-    $("body").click
-        (
-            function (e) {
-                if (e.target.className !== "form-control questioninput") {
-                    $('#questionsblock').addClass('hidequestions');
-                    $('#questionsblock').removeClass('showquestions');
-                }
-            }
-        );
-    $("select").change(function () {
-        $(this).find("option:selected").each(function () {
-            var optionValue = $(this).attr("value");
-            if (optionValue) {
-                $(".box").not("." + optionValue).hide();
-                $("." + optionValue).show();
-            } else {
-                $(".box").hide();
-            }
-        });
-    }).change();
+    $("select")
+        .change(function () {
+            $(this)
+                .find("option:selected")
+                .each(function () {
+                    var optionValue = $(this).attr("value");
+                    if (optionValue) {
+                        $(".box")
+                            .not("." + optionValue)
+                            .hide();
+                        $("." + optionValue).show();
+                    } else {
+                        $(".box").hide();
+                    }
+                });
+        })
+        .change();
 });
 
 function SendAdaptiveCard() {
@@ -49,7 +75,9 @@ function SendAdaptiveCard() {
     list.forEach((obj, i) => {
         htmObj[obj.getAttribute("data-attr")] = obj.value;
     });
-    let index = questions.findIndex(x => x.question === $('#questions-list').val());
+    let index = questions.findIndex(
+        (x) => x.question === $("#questions-list").val()
+    );
     var questionid = null;
     if (index !== -1) {
         questionid = questions[index].questionID;
@@ -57,63 +85,66 @@ function SendAdaptiveCard() {
     }
 
     let taskInfo = {
-        question: $('#questions-list').val(),
+        question: $("#questions-list").val(),
         questionID: questionid,
-        privacy: $('#privacy').val(),
-        executionDate: $('#execdate').val(),
-        executionTime: $('#exectime').val(),
+        privacy: $("#privacy").val(),
+        executionDate: $("#execdate").val(),
+        executionTime: $("#exectime").val(),
         postDate: "",
         isDefaultQuestion: false,
         //postSendNowFlag: true,
-        recurssionType: $('#recurrance').val(),
-        action: "sendAdaptiveCard"
+        recurssionType: $("#recurrance").val(),
+        action: "sendAdaptiveCard",
     };
     taskInfo.card = "";
     taskInfo.height = "medium";
     taskInfo.width = "medium";
-    if (!$('#questions-list').val()) {
-        alert('Please select ' + $('.question').text());
-    }
-    else if (!$('.date-ip').val()) {
-        alert('Please select ' + $('#date').text());
-    }
-    else {
+    if (!$("#questions-list").val()) {
+        alert("Please select " + $(".question").text());
+    } else if (!$(".date-ip").val()) {
+        alert("Please select " + $("#date").text());
+    } else {
         microsoftTeams.tasks.submitTask(taskInfo);
     }
     return true;
-    
 }
 
 function getSelectedOption(event) {
-    $('#selectedTxt').html($('#questions-list').val());
+    $("#selectedTxt").html($("#questions-list").val());
 }
 
 function setPrivacy() {
     microsoftTeams.getContext(function (context) {
-        if (accesstoken !== undefined && accesstoken !== null && accesstoken !== "") {
+        if (
+            accesstoken !== undefined &&
+            accesstoken !== null &&
+            accesstoken !== ""
+        ) {
             GetUserDetails(context.userPrincipalName, accesstoken);
         }
     });
-    $('#privacytext').html($('#privacy').val());
+    $("#privacytext").html($("#privacy").val());
 }
 
 function GetDefaultQuestions(userPrincipleName) {
     var blockdata = "";
     $.ajax({
-        type: 'GET',
-        url: 'api/GetAllDefaultQuestions/' + userPrincipleName,
+        type: "GET",
+        url: "api/GetAllDefaultQuestions/" + userPrincipleName,
         success: function (data) {
             questions = data;
-            data.forEach(x => {
-                blockdata = blockdata + ' <option class="default-opt" id="' + x.questionID + '" value="' + x.question + '">';
-
+            data.forEach((x) => {
+                blockdata =
+                    blockdata +
+                    ' <option class="default-opt" id="' +
+                    x.questionID +
+                    '" value="' +
+                    x.question +
+                    '">';
             });
             $("#questions").html(blockdata);
-
-
-        }
+        },
     });
-
 }
 
 function GetUserDetails(principalName, appAccessToken) {
@@ -124,18 +155,21 @@ function GetUserDetails(principalName, appAccessToken) {
             request.setRequestHeader("Authorization", "Bearer " + appAccessToken);
         },
         success: function (response) {
-            console.log('Success');
+            console.log("Success");
             if (response !== null) {
                 var name = response.displayName;
-                var userNameArray = name.split(' ');
-                $('#usertext').html(" " + userNameArray[0] + " " + (userNameArray[1] !== undefined ? userNameArray[1] : "" ));
+                alert(userNameArray[0]);
+                var userNameArray = name.split(" ");
+                console.log(userNameArray[0]);
+                alert(userNameArray[0]);
+                $("#usertext").html(" " + userNameArray[0] + " " + userNameArray[1]);
             } else {
                 alert("Something went wrong");
             }
         },
         error: function () {
             console.log("Failed");
-        }
+        },
     });
 }
 
@@ -153,12 +187,11 @@ submitHandler = (err, result) => {
     //    microsoftTeams.tasks.startTask(taskInfo, submitHandler)
     //}
     console.log("Reached submithandler!");
-
 };
 
 function openTaskModule() {
     let linkInfo = {
-        action: "ManageRecurringPosts"
+        action: "ManageRecurringPosts",
     };
     microsoftTeams.tasks.submitTask(linkInfo);
     return true;
@@ -166,20 +199,18 @@ function openTaskModule() {
 
 function closeTaskModule() {
     let closeTaskInfo = {
-        action: "closeFirstTaskModule"
+        action: "closeFirstTaskModule",
     };
     microsoftTeams.tasks.submitTask(closeTaskInfo);
     return true;
 }
 
 function addShowHideButton() {
-    if ($('#questionsblock').hasClass('hidequestions')) {
-        $('#questionsblock').removeClass('hidequestions');
-        $('#questionsblock').addClass('showquestions');
-    }
-    else {
-        $('#questionsblock').addClass('hidequestions');
-        $('#questionsblock').removeClass('showquestions');
+    if ($("#questionsblock").hasClass("hidequestions")) {
+        $("#questionsblock").removeClass("hidequestions");
+        $("#questionsblock").addClass("showquestions");
+    } else {
+        $("#questionsblock").addClass("hidequestions");
+        $("#questionsblock").removeClass("showquestions");
     }
 }
-
---
+++ Reflection/wwwroot/Scripts/ManageRecurringPosts.js
@@ -0,0 +1,65 @@
+﻿$(document).ready(function () {
+    microsoftTeams.initialize();
+    microsoftTeams.getContext(function (context) {
+        if (context.theme == "default") {
+            var head = document.getElementsByTagName("head")[0], // reference to document.head for appending/ removing link nodes
+                link = document.createElement("link"); // create the link node
+            link.setAttribute("href", "../CSS/ManageRecurringPosts.css");
+            link.setAttribute("rel", "stylesheet");
+            link.setAttribute("type", "text/css");
+            head.appendChild(link);
+        } else if (context.theme == "dark") {
+            var head = document.getElementsByTagName("head")[0],
+                link = document.createElement("link");
+            link.setAttribute("href", "../CSS/ManageRecurringPosts-dark.css");
+            link.setAttribute("rel", "stylesheet");
+            link.setAttribute("type", "text/css");
+            head.appendChild(link);
+        } else {
+            var head = document.getElementsByTagName("head")[0],
+                link = document.createElement("link");
+            link.setAttribute("href", "../CSS/ManageRecurringPosts-dark.css");
+            link.setAttribute("rel", "stylesheet");
+            link.setAttribute("type", "text/css");
+            head.appendChild(link);
+        }
+    });
+    $(".loader").show();
+    var blockdata = ""
+    $.ajax({
+        type: 'GET',
+        url: 'api/GetRecurssions',
+        success: function (data) {
+            $(".loader").hide();
+            $(".custom-tb").show();
+            recurssions = JSON.parse(JSON.parse(data).recurssions);
+            $("#questioncount").html("(" + recurssions.length + ")");
+            daysInWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+            var sendpostat = "";
+            recurssions.forEach(x => {
+                if (x.RecurssionType == "Monthly") {
+                    sendpostat = "Every Month " + new Date(x.RefCreatedDate).getDate() + " at " + new Date(x.RefCreatedDate).toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' });
+                }
+                else if (x.RecurssionType == "Weekly") {
+                    sendpostat = "Every Week " + daysInWeek[new Date(x.RefCreatedDate).getDay()] + " at " + new Date(x.RefCreatedDate).toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' });
+                }
+                else {
+                    sendpostat = "Every Week Day"
+                }
+                blockdata = blockdata + '<tr id="row1"><td class="hw-r-u">' + x.Question + '<div class="hru-desc">Created by: ' + x.CreatedBy + ' on ' + new Date(x.RefCreatedDate).toDateString() + '</div></td><td class="privacy-cl">' + x.Privacy + '</td> <td class="date-day">' + sendpostat + '</td><td class="delete-icon" data-toggle="modal" data-target="#myalert"></td></tr>';
+            })
+            $("#tablebody").html(blockdata);
+
+        }
+    });
+});
+
+//$(document).on("click", ".delete-icon", function () {
+//    if (confirm('Are you sure to delete ?')) {
+//        $(this).prev('#').remove();
+//    }
+//});
+
+$('.delete-icon').click(function () {
+    $('#myModal').modal('show');
+});
