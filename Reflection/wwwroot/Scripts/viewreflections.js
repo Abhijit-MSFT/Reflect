695a734297a62c5596988e3c4253c7f1310de7b1
Sowjanya-msft <60002014+Sowjanya-msft@users.noreply.github.com>
2020-06-05 23:41:08 +0530

    Update viewreflections.js

--- Reflection/wwwroot/Scripts/viewreflections.js
+++ Reflection/wwwroot/Scripts/viewreflections.js
@@ -3,7 +3,6 @@
 $(document).ready(function () {
     $(".loader").show();
     microsoftTeams.initialize();
-    head.appendChild(link);
     microsoftTeams.getContext(function (context) {
         if (context.theme == "default") {
             var head = document.getElementsByTagName("head")[0], // reference to document.head for appending/ removing link nodes
