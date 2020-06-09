d8b6ced4b2e9256adba0d192424f56a57861cbd1
Sowjanya-msft <60002014+Sowjanya-msft@users.noreply.github.com>
2020-06-09 14:25:58 +0530

    add timings sendlater

--- Reflection/wwwroot/Scripts/Index.js
+++ Reflection/wwwroot/Scripts/Index.js
@@ -1,7 +1,43 @@
 ﻿var questions = [];
 var userobject = {};
 var accesstoken = "";
+var timearray = [];
+var timestring = '';
+var optiondata = '';
 $(document).ready(function () {
+    var presentdate = new Date();
+    var minutes = presentdate.getMinutes();
+    if (minutes < 30) {
+        presentdate.setMinutes(30)
+    }
+    else {
+        presentdate.setHours(presentdate.getHours() + 1)
+        presentdate.setMinutes(00)
+    }
+    var nextdate = new Date(presentdate.toISOString());
+    nextdate.setDate(nextdate.getDate() + 1);
+    while (presentdate < nextdate) {
+        timestring = timestring + (presentdate.getHours() == '0' ? '00' : presentdate.getHours());
+        timestring = timestring + ":";
+        timestring = timestring + (presentdate.getMinutes() == '0' ? '00' : presentdate.getMinutes());
+        timearray.push(timestring);
+        presentdate.setMinutes(presentdate.getMinutes() + 30);
+        timestring = '';
+    }
+    exectime
+    timearray.forEach(x => {
+        var time = '';
+        if (x.split(':')[0] > 12) {
+            var timearray = x.split(':');
+            timearray[0] = timearray[0] - 12;
+            time = timearray.join(':') + ' PM'
+        }
+        else {
+            time =x+' AM'
+        }
+        optiondata = optiondata + '<option val="' + time + '">' + time + '</option>';
+    });
+    $("#exectime").append(optiondata);
     $("#usertext").html(" " + userName);
     microsoftTeams.initialize();
     microsoftTeams.getContext(function (context) {
@@ -27,6 +63,7 @@ $(document).ready(function () {
             link.setAttribute("type", "text/css");
             head.appendChild(link);
         }
+        GetRecurssionsCount(context.userPrincipleName)
         GetDefaultQuestions(context.userPrincipalName);
     });
     $("body").click(function (e) {
@@ -168,6 +205,17 @@ function GetDefaultQuestions(userPrincipleName) {
         },
     });
 }
+function GetRecurssionsCount(userPrincipleName) {
+    $.ajax({
+        type: "GET",
+        url: "api/GetRecurssions/" + userPrincipleName,
+        success: function (data) {
+            recurssions = JSON.parse(JSON.parse(data).recurssions);
+            $("#recurssionscount").html("(" + recurssions.length + ")");
+        },
+    });
+}
+
 
 submitHandler = (err, result) => {
     //if (result.action == "Chaining") {
