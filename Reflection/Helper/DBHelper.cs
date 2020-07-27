c10d9fb387ad2cff31b318c7a506f39d992a633a
Sowjanya-msft <60002014+Sowjanya-msft@users.noreply.github.com>
2020-07-27 21:00:17 +0530

    Update DBHelper.cs

--- Reflection/Helper/DBHelper.cs
+++ Reflection/Helper/DBHelper.cs
@@ -20,6 +20,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
 
 namespace Reflection.Helper
 {
@@ -278,9 +281,13 @@ namespace Reflection.Helper
                             break;
                         }
                         if (recurssionEntity.CustomRecurssionTypeValue.Contains("month"))
-                        {   
-
-                            break;
+                        {
+                            if (recurssionEntity.CustomRecurssionTypeValue.Contains("Day"))
+                            {
+                                goto case "monthly";
+                            }
+                            else
+                                break;
                         }
                         else
                         {
@@ -531,8 +538,71 @@ namespace Reflection.Helper
         }
 
 
+       public  string Encrypt(string text)
+        {
+            var _key = Encoding.UTF8.GetBytes(_configuration["chipher"]);
 
+            using (var aes = Aes.Create())
+            {
+                using (var encryptor = aes.CreateEncryptor(_key, aes.IV))
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (var sw = new StreamWriter(cs))
+                            {
+                                sw.Write(text);
+                            }
+                        }
 
+                        var iv = aes.IV;
+
+                        var encrypted = ms.ToArray();
+
+                        var result = new byte[iv.Length + encrypted.Length];
+
+                        Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
+                        Buffer.BlockCopy(encrypted, 0, result, iv.Length, encrypted.Length);
+
+                        return Convert.ToBase64String(result);
+                    }
+                }
+            }
+        }
+
+        public  string Decrypt(string encrypted)
+        {
+            var b = Convert.FromBase64String(encrypted);
+
+            var iv = new byte[16];
+            var cipher = new byte[16];
+
+            Buffer.BlockCopy(b, 0, iv, 0, iv.Length);
+            Buffer.BlockCopy(b, iv.Length, cipher, 0, iv.Length);
+
+            var _key = Encoding.UTF8.GetBytes(_configuration["chipher"]);
+
+            using (var aes = Aes.Create())
+            {
+                using (var decryptor = aes.CreateDecryptor(_key, iv))
+                {
+                    var result = string.Empty;
+                    using (var ms = new MemoryStream(cipher))
+                    {
+                        using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (var sr = new StreamReader(cs))
+                            {
+                                result = sr.ReadToEnd();
+                            }
+                        }
+                    }
+
+                    return result;
+                }
+            }
+        }
     }
 }
 
