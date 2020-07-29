803946f70e57e6d7fde713f7ce6a7c3667f01238
Sharmila Thanu Moorthi <v-sharmt@microsoft.com>
2020-07-28 19:48:11 -0700

    Style cop for Icard.cs

--- Reflection/Interfaces/ICard.cs
+++ Reflection/Interfaces/ICard.cs
@@ -1,23 +1,28 @@
-﻿
+﻿// -----------------------------------------------------------------------
 // <copyright file="ICard.cs" company="Microsoft">
-// Copyright (c) Microsoft. All rights reserved.
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
 // </copyright>
-
-using AdaptiveCards;
-using Reflection.Model;
-using Reflection.Repositories.FeedbackData;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Threading.Tasks;
+// -----------------------------------------------------------------------
 
 namespace Reflection.Interfaces
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Threading.Tasks;
+    using AdaptiveCards;
+    using Reflection.Model;
+    using Reflection.Repositories.FeedbackData;
+
+    /// <summary>
+    /// Card interface.
+    /// </summary>
     public interface ICard
     {
         AdaptiveCard FeedBackCard(Dictionary<int, List<FeedbackDataEntity>> keyValues, Guid? reflectionId, string questionName);
-        Task<string> saveImage(Image data, string Filepath);
-        AdaptiveCard CreateNewReflect(TaskInfo data);
 
+        Task<string> saveImage(Image data, string Filepath);
+
+        AdaptiveCard CreateNewReflect(TaskInfo data);
     }
 }
