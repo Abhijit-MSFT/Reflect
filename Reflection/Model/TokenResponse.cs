76b623b533f25a585e65f90c5eb8e6a0f71b77f5
Sharmila Thanu Moorthi <v-sharmt@microsoft.com>
2020-07-28 20:35:14 -0700

    style cop update for taskinfo and other files

--- Reflection/Model/TokenResponse.cs
+++ Reflection/Model/TokenResponse.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// -----------------------------------------------------------------------
+// <copyright file="TokenResponse.cs" company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 
 namespace Reflection.Model
 {
+    /// <summary>
+    /// TokenResponse.
+    /// </summary>
     public class TokenResponse
     {
+        /// <summary>
+        /// Gets or sets access_token.
+        /// </summary>
         public string access_token { get; set; }
     }
 }
