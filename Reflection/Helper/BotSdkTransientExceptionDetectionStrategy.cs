c18cdd181b9f063cb4f467707e188648fd42e09f
Sharmila Thanu Moorthi <v-sharmt@microsoft.com>
2020-07-28 18:18:50 -0700

    Style cop for BotSdkTransientExceptionDetectionStrategy.cs

--- Reflection/Helper/BotSdkTransientExceptionDetectionStrategy.cs
+++ Reflection/Helper/BotSdkTransientExceptionDetectionStrategy.cs
@@ -1,19 +1,37 @@
-﻿using Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling;
-using Microsoft.Rest;
-using System;
-using System.Collections.Generic;
+﻿// -----------------------------------------------------------------------
+// <copyright file="BotSdkTransientExceptionDetectionStrategy.cs" company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 
 namespace Reflection.Helper
 {
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling;
+    using Microsoft.Rest;
+
+    /// <summary>
+    /// Bot Sdk Transient Exception Detection Strategy.
+    /// </summary>
     public class BotSdkTransientExceptionDetectionStrategy : ITransientErrorDetectionStrategy
     {
-        // List of error codes to retry on
-        List<int> transientErrorStatusCodes = new List<int>() { 429 };
+        /// <summary>
+        /// List of error codes to retry on.
+        /// </summary>
+        private readonly List<int> transientErrorStatusCodes = new List<int>() { 429 };
 
+        /// <summary>
+        /// Get user feedback.
+        /// </summary>
+        /// <param name="ex">ex.</param>
+        /// <returns>Boolean.</returns>
         public bool IsTransient(Exception ex)
         {
             if (ex.Message.Contains("429"))
+            {
                 return true;
+            }
 
             var httpOperationException = ex as HttpOperationException;
             if (httpOperationException != null)
