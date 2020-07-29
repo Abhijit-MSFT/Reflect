8cfa32d5cd7d7d3f98bb47a5ad4811cf86eda125
Sharmila Thanu Moorthi <v-sharmt@microsoft.com>
2020-07-28 19:15:34 -0700

    Style cop for FeedbackDataRepositoryExtensionHelper.cs

--- Reflection/Helper/FeedbackDataRepositoryExtensionHelper.cs
+++ Reflection/Helper/FeedbackDataRepositoryExtensionHelper.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// -----------------------------------------------------------------------
+// <copyright file="FeedbackDataRepositoryExtensionHelper.cs" company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 
 namespace Reflection.Helper
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// FeedbackDataRepositoryExtension Helper.
+    /// </summary>
     public class FeedbackDataRepositoryExtensionHelper
     {
     }
